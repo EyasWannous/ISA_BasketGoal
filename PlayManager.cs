1274ee6b3a118ca754c03e90e7e97a78917ebc74
EyasWannous <eyaswannous@gmail.com>
2023-11-03 02:11:12 -0700

    Add User Play Mode And Play Manager

---
+++ PlayManager.cs
@@ -0,0 +1,110 @@
+﻿using ISA_BasketGoal.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISA_BasketGoal;
+
+internal class PlayManager
+{
+    private string _playMode;
+    private System.Diagnostics.Stopwatch? _watch;
+    private BFS _bFS = new();
+    private DFS _dFS = new();
+    private List<BoardNode> _finalStates = new();
+    private List<FinalStates> _finals = new();
+    private BoardNode? _boardNode;
+    public PlayManager(string playMode)
+    {
+        _playMode = playMode;
+    }
+    public bool ChooseAPhase(string number)
+    {
+        return number switch
+        {
+            "1" => BuildPhase(1),
+            "2" => BuildPhase(2),
+            "3" => BuildPhase(3),
+            "4" => BuildPhase(4),
+            "5" => BuildPhase(5),
+            "6" => BuildPhase(6),
+            "7" => BuildPhase(7),
+            "8" => BuildPhase(8),
+            "9" => BuildPhase(9),
+            "10" => BuildPhase(10),
+            "11" => BuildPhase(11),
+            "12" => BuildPhase(12),
+            "13" => BuildPhase(13),
+            "14" => BuildPhase(14),
+            "15" => BuildPhase(15),
+            _ => false,
+        };
+    }
+
+    public bool BuildPhase(int phaseNumber)
+    {
+        _boardNode = PhaseManager.LoadPhase(phaseNumber);
+
+        if (_playMode == "User")
+        {
+            UserPlayMode UPM = new(_boardNode);
+            UPM.Play();
+        }
+        else if (_playMode == "DFS")
+        {
+            _watch = System.Diagnostics.Stopwatch.StartNew();
+            _dFS.Solve(_boardNode);
+            _finalStates = _dFS.GetFinalStates();
+            this.FillFinal();
+            PrintAllRoadsToFinalStates(_dFS.BoardNodes);
+            _watch.Stop();
+        }
+        else if (_playMode == "BFS")
+        {
+            _watch = System.Diagnostics.Stopwatch.StartNew();
+            _bFS.Solve(_boardNode);
+            _finalStates = _bFS.GetFinalStates();
+            this.FillFinal();
+            PrintAllRoadsToFinalStates(_bFS.BoardNodes);
+            _watch.Stop();
+        }
+
+        return true;
+    }
+
+    public void FillFinal()
+    {
+        _finalStates.ForEach(state =>
+        {
+            _finals.Add(new(state));
+        });
+    }
+
+    public void PrintAllRoadsToFinalStates(HashSet<BoardNode> hashBoardNode)
+    {
+        int counter = 1;
+        _finals.ForEach(state =>
+        {
+            state.FillMyRoad();
+            state.PrintRoadToFinal();
+
+            Console.WriteLine("========================================");
+            Console.WriteLine($"\tNumber of Final State : {counter}    ");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+
+            counter++;
+        });
+        Console.WriteLine();
+        Console.WriteLine($"Number of States : {hashBoardNode.Count}");
+        Console.WriteLine();
+
+        double elapsed = _watch!.Elapsed.TotalSeconds;
+        long elapsedMS = _watch!.ElapsedMilliseconds;
+
+        Console.WriteLine($"Elapsed time is : {elapsed:N1}s");
+        Console.WriteLine($"Elapsed time is : {elapsedMS:N1}ms");
+    }
+}
---
+++ UserPlayMode.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISA_BasketGoal;
+
+internal class UserPlayMode
+{
+    BoardNode MyBoard { get; set; }
+
+    public UserPlayMode(BoardNode boardNode) => MyBoard = boardNode;
+
+    public void Play()
+    {
+        Console.WriteLine();
+        Console.WriteLine(MyBoard);
+        Console.WriteLine();
+        Console.WriteLine("Press {'W' 'S' 'A' 'D'} To Move.");
+        Console.WriteLine("Press 'M' To See Possialbe Moves.");
+        Console.WriteLine("Press 'P' To Print Board After Every Possialbe Moves.");
+        Console.WriteLine("Press 'Q' To Quit!");
+
+        char Key = ' ';
+        int counter = 0;
+
+        while (Key != 'q' && Key != 'Q' && !MyBoard.IsFinal())
+        {
+            Key = Console.ReadKey(true).KeyChar;
+
+            Console.WriteLine();
+
+            BoardNode temp = new(MyBoard);
+            if (MyBoard.Move(Key))
+            {
+                if (!temp.Equals(MyBoard))
+                { 
+                    Console.WriteLine("Current State :\n");
+                    Console.WriteLine(MyBoard);
+                    counter++;
+                }
+            }
+        }
+
+        if (MyBoard.IsFinal()) Console.WriteLine($"Congratulations, You Won in {counter} Moves");
+
+        else Console.WriteLine("We Are Sorry, You Left");
+    }
+
+}
