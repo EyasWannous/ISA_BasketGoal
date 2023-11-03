0c49a41da3bd637aa0c47d9a3d819fbdec995471
EyasWannous <eyaswannous@gmail.com>
2023-11-03 02:08:53 -0700

    Add Final States

---
+++ FinalStates.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISA_BasketGoal;
+
+class FinalStates : BoardNode
+{
+    public List<BoardNode>? RoadToFinal { get; set; }
+    public FinalStates(BoardNode boardNode) : base(boardNode) { }
+
+    public void FillMyRoad()
+    {
+        RoadToFinal = new() { this };
+
+        BoardNode? temp = this.Father;
+
+        while (temp is not null)
+        {
+            RoadToFinal.Add(temp);
+            temp = temp.Father;
+        }
+
+        RoadToFinal.Reverse();
+    }
+
+    public void PrintRoadToFinal()
+    {
+        if (RoadToFinal is null) return;
+
+        RoadToFinal.ForEach(state =>
+        {
+            Console.WriteLine(state);
+        });
+    }
+}
