ea6d471addf2bcc1d57e5dc0e3d1fed9a85b68fb
EyasWannous <eyaswannous@gmail.com>
2023-11-17 09:53:15 -0700

    Add 'ICostAlgorithm' & 'INoCostAlgorithm'

---
+++ Algorithms/ICostAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISA_BasketGoal.Algorithms;
+
+internal interface ICostAlgorithm
+{
+    public HashSet<BoardNode> BoardNodes { get; set; }
+    void Solve(BoardNode boardNode);
+    List<BoardNode> GetFinalStates();
+}
