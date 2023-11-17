50736b8e0530495a25a5181ce2c5e37eb3b642ac
EyasWannous <eyaswannous@gmail.com>
2023-11-17 09:57:10 -0700

    Make abstract class 'HillClimbingBase'

---
+++ Algorithms/HillClimbingBase.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+namespace ISA_BasketGoal.Algorithms;
+
+internal abstract class HillClimbingBase : ICostAlgorithm
+{
+    public HashSet<BoardNode> BoardNodes { get; set; } = new();
+    protected PriorityQueue<BoardNode, int> NodesQueue { get; set; } = new();
+
+    protected abstract int CalculateCost(BoardNode boardNode);
+
+    public void Solve(BoardNode boardNode)
+    {
+        EnqueueInitialNode(boardNode);
+
+        while (NodesQueue.Count > 0)
+        {
+            BoardNode current = NodesQueue.Dequeue();
+
+            if (BoardNodes.Contains(current))
+                continue;
+
+            BoardNodes.Add(current);
+
+            if (current.IsFinal())
+                return;
+
+            EnqueueChildren(current);
+        }
+    }
+
+    public List<BoardNode> GetFinalStates()
+    {
+        List<BoardNode> finalStates = new();
+
+        foreach (var item in BoardNodes)
+        {
+            if (item.IsFinal())
+                finalStates.Add(item);
+        }
+
+        return finalStates;
+    }
+
+    private void EnqueueInitialNode(BoardNode node)
+    {
+        NodesQueue.Enqueue(node, CalculateCost(node));
+    }
+
+    private void EnqueueChildren(BoardNode parent)
+    {
+        parent.GetChildren();
+
+        if (parent.MyChildren is null)
+            return;
+
+        parent.MyChildren.ForEach(child =>
+        {
+            NodesQueue.Enqueue(child, CalculateCost(child));
+        });
+    }
+
+}
+
