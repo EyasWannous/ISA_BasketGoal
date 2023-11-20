89aa303801a860ea9094b8d9d466efd7825b1fb6
EyasWannous <eyaswannous@gmail.com>
2023-11-20 10:07:35 -0700

    Add 'CostFunctions' Class

---
+++ Algorithms/CostFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISA_BasketGoal.Algorithms;
+
+internal static class CostFunctions
+{
+    public static int WhereIsBalls(BoardNode boardNode)
+    {
+        int count = boardNode.BallPosition.Count * 2;
+        boardNode.BallPosition.ForEach(ball =>
+        {
+            if (ball.Column == boardNode.BasketPosition.Column) count--;
+        });
+        return count;
+    }
+
+    public static int WhereIsBasket(BoardNode boardNode)
+    {
+        if (boardNode.BasketPosition.Row == 0) return boardNode.RowNumbers;
+        else if (boardNode.BasketPosition.Row == boardNode.RowNumbers - 1) return 0;
+        return boardNode.RowNumbers - boardNode.BasketPosition.Row;
+    }
+
+}
