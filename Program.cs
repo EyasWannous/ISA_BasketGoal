57c19e785e05a2f69a3166a2d33a0d7912551e58
EyasWannous <eyaswannous@gmail.com>
2023-11-20 10:10:13 -0700

    Make 'Program.cs' (Main) More Clean

--- Program.cs
+++ Program.cs
@@ -1,29 +1,23 @@
 ﻿using ISA_BasketGoal;
 using System.Text.RegularExpressions;
 
-char Key = ' ';
+string Key = " ";
 
-Console.WriteLine('■');
+Console.WriteLine("■");
 
-while (Key != 'q' && Key != 'Q')
+while (Key != "quit()")
 {
     string? ChosenPhase = "";
 
-    string Keypattern = @"[0-9]";
     bool isValidInput = false;
 
     Write.PlayMode();
+    Write.ChooseOne();
 
-    Key = Console.ReadKey().KeyChar;
+    Key = Console.ReadLine()!;
     Console.WriteLine();
 
-    while (!Regex.IsMatch(Key.ToString(), Keypattern) && Key != 'q')
-    {
-        Write.InvalidKey();
-        Key = Console.ReadKey().KeyChar;
-    }
-
-    if (Key == 'q') continue;
+    if (Key == "quit()") continue;
 
     Write.Phases();
 
@@ -43,27 +37,29 @@ while (Key != 'q' && Key != 'Q')
 
     if (ChosenPhase == "quit()")
     {
-        Key = 'q';
+        Key = "quit()";
         continue;
     }
 
     PlayManager play = Key switch
     {
-        '1' => new("User"),
-        '2' => new("DFS"),
-        '3' => new("BFS"),
-        '4' => new("DFSone"),
-        '5' => new("BFSone"),
-        '6' => new("UNICOST"),
-        '7' => new("HillClimbing_1"),
-        '8' => new("HillClimbing_2"),
-        '9' => new("HillClimbing_3"),
-        '0' => new("AAsterisk"),
+        "1" => new("User"),
+        "2" => new("DFS"),
+        "3" => new("BFS"),
+        "4" => new("DFSone"),
+        "5" => new("BFSone"),
+        "6" => new("UNICOST"),
+        "7" => new("HillClimbing_1"),
+        "8" => new("HillClimbing_2"),
+        "9" => new("HillClimbing_3"),
+        "10" => new("AAsterisk_1"),
+        "11" => new("AAsterisk_2"),
+        "12" => new("AAsterisk_3"),
         _ => new("User"),
     };
 
     play.ChooseAPhase(ChosenPhase!);
 
     Write.Repeat();
-    Key = Console.ReadKey().KeyChar;
+    Key = Console.ReadLine()!;
 }
